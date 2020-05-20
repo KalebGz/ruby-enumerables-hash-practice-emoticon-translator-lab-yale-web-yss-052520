ce567ba56e6d037f980e2795244269fd569c3225
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-20 21:25:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -39,7 +39,7 @@ def get_english_meaning(file_path, emote )
   res = ""
   hash = emoticons.each{|key, value|
   if(emoticons[key][:japanese] == emote)
-    res = emoticons[key]
+    res = key
   end
   }
   res
