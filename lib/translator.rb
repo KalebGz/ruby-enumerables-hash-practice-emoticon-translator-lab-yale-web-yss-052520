1bb549d9d1c25a20fedbdbe3654497949134c584
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-20 21:08:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,44 @@
 # require modules here
+require "pry"
+require "yaml"
 
-def load_library
+def load_library(file_path)
   # code goes here
+  
+hash = YAML.load_file(file_path)
+new_hash = {}
+# new_hash[:english] = {}
+# new_hash[:japanese] = {}
+hash.each{|key, value|
+new_hash[key] = {}
+new_hash[:japanese][value.first] =  value.last
+new_hash[:english][value.last] =  key
+}
+binding.pry
+new_hash
 end
+
+# res = data.each_with_object({}){ |(key, value), final_hash|
+#     value.each{|inner_key, names|
+#       names.each{ |name|
+#         if !final_hash[name]
+#           final_hash[name] = {}
+#         end
+#         if !final_hash[name][key]
+#           final_hash[name][key] = []
+#         end
+#         final_hash[name][key].push(inner_key.to_s)
+#       }
+#     }
+#   }
+
 
 def get_japanese_emoticon
   # code goes here
+  emoticons = load_library(emoticons.YAML)
 end
 
 def get_english_meaning
   # code goes here
+  emoticons = load_library
 end
